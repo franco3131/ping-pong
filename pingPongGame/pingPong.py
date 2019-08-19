c7087c0f2d51d11d56c1c4836bd0007b248f948b
davidfranco <davidfranco@davids-mbp.lan1>
2019-08-18 19:32:47 -0700

    adding functions to make it more readable. I am continuing to add more functions and descritptive variables.

--- pingPongGame/pingPong.py
+++ pingPongGame/pingPong.py
@@ -1,9 +1,11 @@
-# import the pygame module
 import pygame
-import random
+from pygame.locals import *
+
+
 
 playerX=740   
 playerY=100
+
 cpuX=30
 cpuY=0
 height=0
@@ -20,19 +22,80 @@ cpuHeight=100
 
 
 # import pygame.locals for easier access to key coordinates
-from pygame.locals import *
 
 # Define our player object and call super to give it all the properties and methods of pygame.sprite.Sprite
 # The surface we draw on the screen is now a property of 'player'
-class Player(pygame.sprite.Sprite):
+class Entities(pygame.sprite.Sprite):
     def __init__(self):
-        super(Player, self).__init__()
-        self.surf = pygame.Surface((25, 100))
-        self.surf.fill((255, 255, 255))
-        self.rect = self.surf.get_rect()
-        self.surfs = pygame.Surface((25, 100))
-        self.surfs.fill((255, 255, 255))
-        self.rect = self.surfs.get_rect()
+        super(Entities, self).__init__()
+        self.cpuSurface = pygame.Surface((25, 100))
+        self.cpuSurface.fill((255, 255, 255))
+        self.rect = self.cpuSurface.get_rect()
+        
+        self.playerSurface = pygame.Surface((25, 100))
+        self.playerSurface.fill((255, 255, 255))
+        self.rect = self.playerSurface.get_rect()
+        
+        self.playerHP=500
+        self.cpuHP=500
+        
+        self.playerX=740
+        self.playerY=100
+        
+        
+        
+        
+        
+    def scoreDisplay(self):
+            font = pygame.font.Font('freesansbold.ttf', 32) 
+            screen.fill(pygame.Color("black")) # erases the entire screen surface
+        
+            textsurface = font.render("CPU :"+str(self.getCPUHpScore()), False, (255, 255, 255))
+            screen.blit(textsurface,(0,0))
+            
+            textsurface = font.render("player :"+str(self.getPlayerHpScore()), False, (255, 255, 255))
+            screen.blit(textsurface,(600,0))
+    
+    
+    def setPlayerHPScore(self,playerHP_):
+            self.playerHP=playerHP_
+        
+    def getPlayerHpScore(self):
+            return self.playerHP;
+        
+    def setCPUHPScore(self,cpuHP_):
+            self.cpuHP=cpuHP_
+        
+    def getCPUHpScore(self):
+            return cpuHP;  
+        
+    def clearPlayerImage(self): 
+            surf3 = pygame.Surface((25, 100))
+            surf3.fill((0, 0, 0))
+            screen.blit(surf3, (self.getPlayerPositionX(), self.getPlayerPositionY()))
+    def drawPlayerImage(self):
+            screen.blit(self.playerSurface, (self.getPlayerPositionX(),self.getPlayerPositionY()))
+            self.playerSurface.fill((255, 255, 255))
+            pygame.display.update()
+            
+        
+    def getPlayerPositionX(self):
+        return self.playerX 
+    def setPlayerPositionX(self,playerX_):
+        self.playerX=playerX_
+    def getPlayerPositionY(self):
+        return self.playerY
+    def setPlayerPositionY(self,playerY_):
+        self.playerY=playerY_ 
+    def setCpuPositionY(self,cpuY_):
+        self.cpuY=cpuY_
+    def getCpuPositionY(self):
+        return self.cpuY
+    def getCpuVelocityY(self):
+        return self.cpuVy
+    def setCpuVelocityY(self,cpuVy_):
+        self.cpuVy=cpuVy_
+    
 
 # initialize pygame
 pygame.init()
@@ -42,11 +105,12 @@ pygame.init()
 screen = pygame.display.set_mode((800, 600))
 
 # instantiate our player; right now he's just a rectangle
-player = Player()
+entities = Entities()
 clock = pygame.time.Clock()
 
 # Variable to keep our main loop running
 running = True
+
 
 
 
@@ -60,26 +124,19 @@ while running:
             if event.key == K_ESCAPE:
                 running = False
             elif event.key == pygame.K_UP:
-                surf3 = pygame.Surface((25, 100))
-                surf3.fill((0, 0, 0))
-                screen.blit(surf3, (playerX, playerY))
-                playerY=playerY-40
-          
+                entities.clearPlayerImage();
+                entities.setPlayerPositionY(entities.getPlayerPositionY()-40)
+              
             elif event.key == pygame.K_DOWN:
-                surf3 = pygame.Surface((25, 100))
-                surf3.fill((0, 0, 0))
-                screen.blit(surf3, (playerX, playerY))
-                playerY=playerY+40
-             
+                entities.clearPlayerImage()
+                entities.setPlayerPositionY(entities.getPlayerPositionY()+40)
+      
         # Check for QUIT event; if QUIT, set running to false
         elif event.type == QUIT:
             running = False
        
-    # Draw the player to the screen
-    surf2 = pygame.Surface((25, 100))
-    surf2.fill((0, 0, 0))
-    screen.blit(surf2, (cpuX, cpuY))
-    r=random.randint(0, 5)
+ 
+  
     
 
     
@@ -89,9 +146,12 @@ while running:
     if circleVx+circleX>=800  :
         circleVx*=-1
         playerHP=playerHP-20
+        entities.setPlayerHPScore(playerHP)
+        
     elif circleVx+circleX<=0:
         circleVx*=-1
         cpuHP=cpuHP-20
+        entities.setCPUHPScore(cpuHP)
     elif (circleVx+circleX+radius>=playerX and (circleVy+circleY>=playerY and circleVy+circleY<=(playerY+100))) or (circleX+circleVx<=cpuX+25 and (circleVy+circleY>=cpuY and circleVy+circleY<=(cpuY+100))):
         circleVx*=-1
     elif circleVy+circleY>=600 or circleVy+circleY<=0:
@@ -131,15 +191,7 @@ while running:
         cpuVy*=-1
     
    
-   
-    font = pygame.font.Font('freesansbold.ttf', 32) 
-    screen.fill(pygame.Color("black")) # erases the entire screen surface
-
-    textsurface = font.render("CPU :"+str(cpuHP), False, (255, 255, 255))
-    screen.blit(textsurface,(0,0))
-    
-    textsurface = font.render("player :"+str(playerHP), False, (255, 255, 255))
-    screen.blit(textsurface,(600,0))
+    entities.scoreDisplay()
     
  
    
@@ -151,16 +203,14 @@ while running:
         
     pygame.draw.circle(screen, (255,255,255), (circleX, circleY), radius, 10)
   
-    surf3 = pygame.Surface((25, 100))
-    surf3.fill((0, 0, 0))
-    screen.blit(surf3, (playerX, playerY))
-    screen.blit(player.surfs, (playerX, playerY))
-    player.surfs.fill((255, 255, 255))
-    pygame.display.update()
-    screen.blit(player.surf, (cpuX, cpuY))
+ 
+    entities.drawPlayerImage()
+    
+    screen.blit(entities.cpuSurface, (cpuX, cpuY))
     pygame.display.update()
 
-     
+
+
       
     # Update the display
     pygame.display.flip()
